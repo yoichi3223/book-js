7a11e9f66ef3488b1cdfe395f758fef611b3f28d
yoichihashimoto <yoichi3223@gmail.com>
2020-11-16 14:09:03 +0900

    Api2 add

--- practice/7-02_api/script.js
+++ practice/7-02_api/script.js
@@ -10,6 +10,9 @@ function fail(error){
 navigator.geolocation.getCurrentPosition(success,fail);
 
 //データ取得
+function utcToJSTime(utcTime){
+  return utcTime * 1000;
+}
 
 function ajaxRequest(lat,long){
 const url = 'https://api.openweathermap.org/data/2.5/forecast'
@@ -28,6 +31,24 @@ lang: 'ja'
 
 .done(function(data){
 console.log(data);
+console.log('都市名'+data.city.name);
+console.log('都市名'+data.city.country);
+
+data.list.forEach(function(forecast, index) {
+            const dateTime = new Date(utcToJSTime(forecast.dt));
+            const month = dateTime.getMonth() + 1;
+            const date = dateTime.getDate();
+            const hours = dateTime.getHours();
+            const min = String(dateTime.getMinutes()).padStart(2, '0');
+            const temperature = Math.round(forecast.main.temp);
+            const description = forecast.weather[0].description;
+            const iconPath = `images/${forecast.weather[0].icon}.svg`;
+
+            console.log('日時：' + `${month}/${date} ${hours}:${min}`);
+            console.log('気温：' + temperature);
+            console.log('天気：' + description);
+            console.log('画像パス：' + iconPath);
+        });
 })
 .fail(function(){
 console.log('$.ajax failed!');
